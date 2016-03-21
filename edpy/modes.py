bd6168d9611394dba00d3af9c8a7d453044a77b3
mmanguno <mmanguno3@gatech.edu>
2016-03-21 02:16:44 -0400

    Begin adding modes; now automatically list modes.
    Wrote the mode superclass and the quit mode. No longer need to enumerate all
    modes in the parser, now do it automatically through reflection.

---
+++ edpy/modes.py
@@ -0,0 +1,75 @@
+import sys
+
+"""modes.py: the modes of edpy"""
+
+
+# Define an error message to use on all Mode superclass methods
+abstract_implemented_error = "Mode is abstract; function not implemented."
+
+
+class Mode(object):
+    """The mode superclass. It's abstract: all functions throw an exception.
+
+    For documentation purposes, each function in the abstract superclass will
+    be fleshed out with documentation and full arguments. It is a template;
+    look to it for the necessary structure of modes.
+    """
+
+    def __init__(self, pre_args, post_args):
+        """Initialize a mode with arguments.
+
+        Keyword arguments:
+        pre_args -- the arguments appearing before the mode identifier
+        post_args -- the arguments appearing after the mode identifier
+
+        Returns the mode.
+        """
+        raise NotImplementedError(abstract_implemented_error)
+
+    def run(self):
+        """'Run' the mode. This it the main run-loop for the mode to enter.
+
+        When run is called, the mode should begin its function. For example,
+        when InsertMode is called, the run function will be a main loop that
+        allows the user to insert text, and ends appropriately.
+        """
+        raise NotImplementedError(abstract_implemented_error)
+
+    def getIdentifier(self):
+        """Return the single letter identifier of the mode.
+
+        This is useful to enforce each mode having an identifier. It could
+        simply be a class variable, but then that doesn't necassarily make it
+        required; having this method makes it so. Use this when getting the
+        identifier.
+
+        Returns the identifier of the mode.
+        """
+        raise NotImplementedError(abstract_implemented_error)
+
+
+class QuitMode(Mode):
+    """The 'mode' for quitting. Exits the program when called."""
+
+    def __init__(self, pre_args, post_args):
+        """Initializes a QuitMode.
+
+        Keyword arguments:
+        pre_args -- the arguments appearing before the mode identifier
+        post_args -- the arguments appearing after the mode identifier
+
+        Returns a QuitMode object.
+        """
+        self.pre_args = pre_args
+        self.post_args = post_args
+
+    def run(self):
+        """Exit the program."""
+        sys.exit()
+
+    def getIdentifer(self):
+        """Return 'q', the identifier of QuitMode.
+
+        Returns 'q'.
+        """
+        return 'q'
--- edpy/parser.py
+++ edpy/parser.py
@@ -1,13 +1,27 @@
-# Declare the major modes
-modes = {'e': EditMode,
-         'i': InsertMode,
-         'p': PrintMode,
-         'n': NumberedMode
-         'q': QuitMode}
+from inspect import getmembers, isclass
+import modes
 
+# Declare the major modes
+modes = _dict_modes()
+
+def _dict_modes():
+    """Put modes in a dict of {identifier: mode_class}.
+
+    Returns the mode dictionary.
+    """
+    superclass = modes.Mode  # Don't want to include the superclass in dict
+    all_members = getmembers(modes)  # Get all top-level members
+
+    mode_dict = {}
+    for member in all_members: # For all members in the module...
+        if isclass(member):  # If member is a class (not variable)
+            if member is not superclass: # Don't include superclass
+                mode_dict[member.getIdentifier(): member]  # Add to dict
+
+    return mode_dict
 
 def parse(args):
-    """Parses the input string, and sends it to the correct mode handler.
+    """Parse the input string, and send it to the correct mode handler.
 
     Keyword arguments:
     args -- a valid ed input string
@@ -26,7 +40,7 @@ def parse(args):
 
 
 def loop():
-    """Runs the loop: get args, run args, get args, run args, etc."""
+    """Run the loop: get args, run args, get args, run args, etc."""
     # Run forever. A call to QuitMode will be the only way to exit.
     while(True):
         args = input("")
